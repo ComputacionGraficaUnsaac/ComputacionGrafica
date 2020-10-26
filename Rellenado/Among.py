ab7f518edae8529f250449dd2dc86207ef867c82
Calderon Tintaya Fallcha Xiomara <72477028+Fallcha@users.noreply.github.com>
2020-10-25 21:02:22 -0500

    Update Among.py

--- Rellenado/Among.py
+++ Rellenado/Among.py
@@ -3,34 +3,28 @@
 # pip install pygame==2.0.0.dev6 (for python 3.8.x)
 # Python 3.8
 from utils import *
-
-def main():
+from transform import *
+def among():
 	scale = 3
 	width, height = scale * 200, scale * 200
-
+    #iniciando pantalla
 	pygame.init()
 	pygame.display.set_caption('AHOGADO')
 	
 	display_openGL(width, height, scale)
-	# glColor3f(1.0, 0, 0)
-	x = 0
-	y = 0
-	set_pixel(x, y, 1, 1, 1, scale)
-	
-	#CUADRILATERO - 4 vertices
+
+	#vertices de cada figura a utilizar para realizar personaje 
 	verticeslente = [(-50, 35), (-40, 40), (0, 40),(0,10),(-40,10),(-50,15)]
 	verticescuerpo = [(-20,60), (20, 60), (20, 50),(30,50),(30,-40),(10,-40),(10,-30),(-10,-30),(-10,-40),(-30,-40),(-30,10),(0,10),(0,40),(-30,40),(-30,50),(-20,50)]
 	verticesmochila = [(30,40), (50, 40), (50,-10), (30,-10)]
-	#TRIANGULO - 3 vertices
-	#vertices = [(-20, -10), (20, 20), (10, -20)]
-	#POLIGONO - CHACANA
-	#vertices = [(40, 20), (30, 20), (30, 30), (20, 30), (20, 40), (-20, 40), (-20, 30), (-30, 30), (-30, 20), (-40, 20),(-40, -20), (-30, -20), (-30, -30), (-20, -30), (-20, -40),(20, -40), (20, -30), (30, -30), (30, -20), (40, -20)]
-
-	#punto (0,0) dentro de la figura --> pixel semilla
+	
+	#punto (-30,25) dentro de la figura --> pixel semilla del lente
 	xi = -30
 	yi = 25
+	#punto (0,0) dentro de la figura --> pixel semilla del cuerpo
 	xi1 = 0
 	yi1 = 0
+	#punto (40,20) dentro de la figura --> pixel semilla de la mochila
 	xi2 = 40
 	yi2 = 20
 	DrawPolygon(verticescuerpo,  255/255, 255/255, 255/255, 3)
@@ -38,26 +32,259 @@ def main():
 	DrawPolygon(verticesmochila,  255/255, 255/255, 255/255, 3)
 	SimpleSeedFill(width, height, 1, verticescuerpo, xi1, yi1, 255/255, 0/255, 0/255)
 	SimpleSeedFill(width, height, 1, verticesmochila,xi2, yi2, 255/255, 0/255, 0/255)
-	SimpleSeedFill(width, height, 1, verticeslente, xi, yi, 204/255, 229/255, 255/255)
+	SimpleSeedFill(width, height, 1, verticeslente, xi, yi, 0/255, 255/255, 255/255)
 
-	#SimpleSeedFill(width, height, 1, vertices, xi, yi, 0/255, 255/255, 255/255)
-	#SimpleSeedFill(width, height, 1, vertices, xi, yi, 255/255, 255/255, 0/255)
-	#SimpleSeedFill(width, height, 1, vertices1, xi1, yi1, 255/255, 255/255, 0/255)
-	# rgb = glReadPixels(width/2 +x-1, height/2+y-1, scale, scale, GL_RGB, GL_UNSIGNED_BYTE, None)
-	# print(list(rgb))
-
-	# Fill Triangle
-	#vertices = [(1, 1), (40, 40), (80, 1)]
-	#FillTriangle(vertices, 255/255, 0/255, 0/255, scale)
-	#DrawPolygon(vertices, 255/255, 0/255, 0/255, scale)
 	print("Finish...")
-	glFlush()
+	#glFlush()
 	pygame.display.flip()
 	 
 	while True:
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				return
+def avanzar0among():
+	scale = 3
+	width, height = scale * 200, scale * 200
 
+	pygame.init()
+	pygame.display.set_caption('C.G. I')
+	
+	display_openGL(width, height, scale)
+	x = 0
+	y = 0
+	set_pixel(x, y, 255/255, 255/255, 255/255, scale)
+	verticeslente = [[-50, 35, 1], [-40, 40, 1], [0, 40, 1],[0,10, 1],[-40,10, 1],[-50,15, 1]]
+	verticesmochila=[[30,40, 1], [50, 40, 1], [50,-10, 1],[30,-10, 1]]
+	verticescuerpo=[[-20,60, 1], [20, 60, 1], [20, 50, 1],[30,50, 1],[30,-40, 1],[10,-40, 1], [10,-30, 1], [-10,-30, 1],[-10,-40, 1],[-30,-40, 1],[-30,10, 1], [0,10, 1], [0,40, 1],[-30,40, 1],[-30,50, 1],[-20,50, 1]]
+	DrawPolygon_(verticeslente, 255/255, 0/255, 0/255, scale)
+	DrawPolygon_(verticescuerpo, 255/255, 0/255, 0/255, scale)
+	DrawPolygon_(verticesmochila, 255/255, 0/255, 0/255, scale)
+	tx = -300
+	ty = 0
+	verticeslente2 = Traslate(verticeslente, tx, ty)
+	verticescuerpo2 = Traslate(verticescuerpo, tx, ty)
+	verticesmochila2 = Traslate(verticesmochila, tx, ty)
+	print(verticeslente2)
+	print(verticescuerpo2)
+	print(verticesmochila2)
+	DrawPolygon_(verticeslente2, 0/255, 255/255, 0/255, scale)
+	DrawPolygon_(verticescuerpo2, 0/255, 255/255, 0/255, scale)
+	DrawPolygon_(verticesmochila2, 0/255, 255/255, 0/255, scale)
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				return
+def avanzar1among():
+	scale = 3
+	width, height = scale * 200, scale * 200
+
+	pygame.init()
+	pygame.display.set_caption('C.G. I')
+	
+	display_openGL(width, height, scale)
+	x = 0
+	y = 0
+	set_pixel(x, y, 255/255, 255/255, 255/255, scale)
+	verticeslente = [[-50, 35, 1], [-40, 40, 1], [0, 40, 1],[0,10, 1],[-40,10, 1],[-50,15, 1]]
+	verticesmochila=[[30,40, 1], [50, 40, 1], [50,-10, 1],[30,-10, 1]]
+	verticescuerpo=[[-20,60, 1], [20, 60, 1], [20, 50, 1],[30,50, 1],[30,-40, 1],[10,-40, 1], [10,-30, 1], [-10,-30, 1],[-10,-40, 1],[-30,-40, 1],[-30,10, 1], [0,10, 1], [0,40, 1],[-30,40, 1],[-30,50, 1],[-20,50, 1]]
+	DrawPolygon_(verticeslente, 255/255, 0/255, 0/255, scale)
+	DrawPolygon_(verticescuerpo, 255/255, 0/255, 0/255, scale)
+	DrawPolygon_(verticesmochila, 255/255, 0/255, 0/255, scale)
+	tx = 300
+	ty = 0
+	verticeslente2 = Traslate(verticeslente, tx, ty)
+	verticescuerpo2 = Traslate(verticescuerpo, tx, ty)
+	verticesmochila2 = Traslate(verticesmochila, tx, ty)
+	print(verticeslente2)
+	print(verticescuerpo2)
+	print(verticesmochila2)
+	DrawPolygon_(verticeslente2, 0/255, 255/255, 0/255, scale)
+	DrawPolygon_(verticescuerpo2, 0/255, 255/255, 0/255, scale)
+	DrawPolygon_(verticesmochila2, 0/255, 255/255, 0/255, scale)
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				return
+def rotaramong():
+	scale = 3
+	width, height = scale * 200, scale * 200
+
+	pygame.init()
+	pygame.display.set_caption('C.G. I')
+	
+	display_openGL(width, height, scale)
+	x = 0
+	y = 0
+	set_pixel(x, y, 255/255, 255/255, 255/255, scale)
+	verticeslente = [[-50, 35, 1], [-40, 40, 1], [0, 40, 1],[0,10, 1],[-40,10, 1],[-50,15, 1]]
+	verticesmochila=[[30,40, 1], [50, 40, 1], [50,-10, 1],[30,-10, 1]]
+	verticescuerpo=[[-20,60, 1], [20, 60, 1], [20, 50, 1],[30,50, 1],[30,-40, 1],[10,-40, 1], [10,-30, 1], [-10,-30, 1],[-10,-40, 1],[-30,-40, 1],[-30,10, 1], [0,10, 1], [0,40, 1],[-30,40, 1],[-30,50, 1],[-20,50, 1]]
+	DrawPolygon_(verticeslente, 255/255, 0/255, 0/255, scale)
+	DrawPolygon_(verticescuerpo, 255/255, 0/255, 0/255, scale)
+	DrawPolygon_(verticesmochila, 255/255, 0/255, 0/255, scale)
+	angle = 90
+	verticeslente2 = Rotation(verticeslente, angle)
+	verticescuerpo2 = Rotation(verticescuerpo, angle)
+	verticesmochila2 = Rotation(verticesmochila, angle)
+	print(verticeslente2)
+	print(verticescuerpo2)
+	print(verticesmochila2)
+	DrawPolygon_(verticeslente2, 0/255, 255/255, 0/255, scale)
+	DrawPolygon_(verticescuerpo2, 0/255, 255/255, 0/255, scale)
+	DrawPolygon_(verticesmochila2, 0/255, 255/255, 0/255, scale)
+
+def rellenaragua1():
+	scale = 3
+	width, height = scale * 200, scale * 200
+    #iniciando pantalla
+	pygame.init()
+	pygame.display.set_caption('AHOGADO')
+	
+	display_openGL(width, height, scale)
+
+	#vertices de cada figura a utilizar para realizar personaje 
+	verticeslente = [(-50, 35), (-40, 40), (0, 40),(0,10),(-40,10),(-50,15)]
+	verticescuerpo = [(-20,60), (20, 60), (20, 50),(30,50),(30,-40),(10,-40),(10,-30),(-10,-30),(-10,-40),(-30,-40),(-30,10),(0,10),(0,40),(-30,40),(-30,50),(-20,50)]
+	verticesmochila = [(30,40), (50, 40), (50,-10), (30,-10)]
+	
+	#punto (-30,25) dentro de la figura --> pixel semilla del lente
+	xi = -30
+	yi = 25
+	#punto (0,0) dentro de la figura --> pixel semilla del cuerpo
+	xi1 = 0
+	yi1 = 0
+	#punto (40,20) dentro de la figura --> pixel semilla de la mochila
+	xi2 = 40
+	yi2 = 20
+	DrawPolygon(verticescuerpo,  255/255, 255/255, 255/255, 3)
+	DrawPolygon(verticeslente,  255/255, 255/255, 255/255, 3)
+	DrawPolygon(verticesmochila,  255/255, 255/255, 255/255, 3)
+	SimpleSeedFill(width, height, 1, verticescuerpo, xi1, yi1, 255/255, 0/255, 0/255)
+	SimpleSeedFill(width, height, 1, verticesmochila,xi2, yi2, 0/255, 0/255, 0/255)
+	SimpleSeedFill(width, height, 1, verticeslente, xi, yi, 0/255, 255/255, 255/255)
+
+	print("Finish...")
+	#glFlush()
+	pygame.display.flip()
+	 
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				return
+def rellenaragua2():
+	scale = 3
+	width, height = scale * 200, scale * 200
+    #iniciando pantalla
+	pygame.init()
+	pygame.display.set_caption('AHOGADO')
+	
+	display_openGL(width, height, scale)
+
+	#vertices de cada figura a utilizar para realizar personaje 
+	verticeslente = [(-50, 35), (-40, 40), (0, 40),(0,10),(-40,10),(-50,15)]
+	verticescuerpo = [(-20,60), (20, 60), (20, 50),(30,50),(30,-40),(10,-40),(10,-30),(-10,-30),(-10,-40),(-30,-40),(-30,10),(0,10),(0,40),(-30,40),(-30,50),(-20,50)]
+	verticesmochila = [(30,40), (50, 40), (50,-10), (30,-10)]
+	
+	#punto (-30,25) dentro de la figura --> pixel semilla del lente
+	xi = -30
+	yi = 25
+	#punto (0,0) dentro de la figura --> pixel semilla del cuerpo
+	xi1 = 0
+	yi1 = 0
+	#punto (40,20) dentro de la figura --> pixel semilla de la mochila
+	xi2 = 40
+	yi2 = 20
+	DrawPolygon(verticescuerpo,  255/255, 255/255, 255/255, 3)
+	DrawPolygon(verticeslente,  255/255, 255/255, 255/255, 3)
+	DrawPolygon(verticesmochila,  255/255, 255/255, 255/255, 3)
+	SimpleSeedFill(width, height, 1, verticescuerpo, xi1, yi1, 0/255, 0/255, 0/255)
+	SimpleSeedFill(width, height, 1, verticesmochila,xi2, yi2, 0/255, 0/255, 0/255)
+	SimpleSeedFill(width, height, 1, verticeslente, xi, yi, 0/255, 255/255, 255/255)
+
+	print("Finish...")
+	#glFlush()
+	pygame.display.flip()
+	 
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				return
+def rellenaragua1():
+	scale = 3
+	width, height = scale * 200, scale * 200
+    #iniciando pantalla
+	pygame.init()
+	pygame.display.set_caption('AHOGADO')
+	
+	display_openGL(width, height, scale)
+
+	#vertices de cada figura a utilizar para realizar personaje 
+	verticeslente = [(-50, 35), (-40, 40), (0, 40),(0,10),(-40,10),(-50,15)]
+	verticescuerpo = [(-20,60), (20, 60), (20, 50),(30,50),(30,-40),(10,-40),(10,-30),(-10,-30),(-10,-40),(-30,-40),(-30,10),(0,10),(0,40),(-30,40),(-30,50),(-20,50)]
+	verticesmochila = [(30,40), (50, 40), (50,-10), (30,-10)]
+	
+	#punto (-30,25) dentro de la figura --> pixel semilla del lente
+	xi = -30
+	yi = 25
+	#punto (0,0) dentro de la figura --> pixel semilla del cuerpo
+	xi1 = 0
+	yi1 = 0
+	#punto (40,20) dentro de la figura --> pixel semilla de la mochila
+	xi2 = 40
+	yi2 = 20
+	DrawPolygon(verticescuerpo,  255/255, 255/255, 255/255, 3)
+	DrawPolygon(verticeslente,  255/255, 255/255, 255/255, 3)
+	DrawPolygon(verticesmochila,  255/255, 255/255, 255/255, 3)
+	SimpleSeedFill(width, height, 1, verticescuerpo, xi1, yi1, 255/255, 0/255, 0/255)
+	SimpleSeedFill(width, height, 1, verticesmochila,xi2, yi2, 0/255, 0/255, 0/255)
+	SimpleSeedFill(width, height, 1, verticeslente, xi, yi, 0/255, 255/255, 255/255)
+
+	print("Finish...")
+	#glFlush()
+	pygame.display.flip()
+	 
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				return
+def rellenaragua2():
+	scale = 3
+	width, height = scale * 200, scale * 200
+    #iniciando pantalla
+	pygame.init()
+	pygame.display.set_caption('AHOGADO')
+	
+	display_openGL(width, height, scale)
+
+	#vertices de cada figura a utilizar para realizar personaje 
+	verticeslente = [(-50, 35), (-40, 40), (0, 40),(0,10),(-40,10),(-50,15)]
+	verticescuerpo = [(-20,60), (20, 60), (20, 50),(30,50),(30,-40),(10,-40),(10,-30),(-10,-30),(-10,-40),(-30,-40),(-30,10),(0,10),(0,40),(-30,40),(-30,50),(-20,50)]
+	verticesmochila = [(30,40), (50, 40), (50,-10), (30,-10)]
+	
+	#punto (-30,25) dentro de la figura --> pixel semilla del lente
+	xi = -30
+	yi = 25
+	#punto (0,0) dentro de la figura --> pixel semilla del cuerpo
+	xi1 = 0
+	yi1 = 0
+	#punto (40,20) dentro de la figura --> pixel semilla de la mochila
+	xi2 = 40
+	yi2 = 20
+	DrawPolygon(verticescuerpo,  255/255, 255/255, 255/255, 3)
+	DrawPolygon(verticeslente,  255/255, 255/255, 255/255, 3)
+	DrawPolygon(verticesmochila,  255/255, 255/255, 255/255, 3)
+	SimpleSeedFill(width, height, 1, verticescuerpo, xi1, yi1, 0/255, 225/255, 225/255)
+	SimpleSeedFill(width, height, 1, verticesmochila,xi2, yi2, 0/255,225/255, 225/255)
+	SimpleSeedFill(width, height, 1, verticeslente, xi, yi, 0/255, 0/255, 0/255)
+
+	print("Finish...")
+	#glFlush()
+	pygame.display.flip()
+	 
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				return
 if __name__ == '__main__':
-	main()
+	among()
