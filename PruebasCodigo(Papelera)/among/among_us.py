a5683fcda86917832c929d44ef9538885fd321d5
MLuAnc <72520284+MLuAnc@users.noreply.github.com>
2020-10-30 21:54:53 -0500

    Create among_us.py

---
+++ PruebasCodigo(Papelera)/among/among_us.py
@@ -0,0 +1,13 @@
+from draw_pixel import Pixel
+from coordenadas import *
+
+among_us = Pixel()
+
+
+among_us.draw(coordenada1,"#080012")
+among_us.draw(coordenada2,"#6736ad")
+among_us.draw(coordenada3,"#411779")
+among_us.draw(coordenada4,"#1d91bf")
+
+
+among_us.finish()
